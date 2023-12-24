a703f9d0df6311810d145fe911b92ea7e8acc828
jaywagh7 <jayeshwagh665@gmail.com>
2023-12-24 19:15:55 +0530

    add files

---
+++ footer.php
@@ -0,0 +1,34 @@
+
+<footer id="footer">
+		<div class="cols">
+				<div class="col media">
+				  
+					<h3>Social media</h3>
+					<ul class="social">
+						<li><a href="#"><span class="ico ico-fb"></span>Facebook</a></li>
+						<li><a href="#"><span class="ico ico-tw"></span>Twitter</a></li>
+						<li><a href="#"><span class="ico ico-gp"></span>Google+</a></li>
+						<li><a href="#"><span class="ico ico-pi"></span>Pinterest</a></li>
+					</ul>
+				
+				</div>
+				<div class="col contact">
+				
+					<h3>Contact us</h3>
+					<p>plant STORE<br>Gujrathi Complex<br>SHIRPUR</p>
+					<p><span class="ico ico-em"></span><a href="#">contact@plantstore.com</a></p>
+					<p><span class="ico ico-ph"></span>(+91) 8600535312</p>
+				</div>
+				<div class="col payment">
+					<h3>Payment System</h3>
+					<img src="images/cash.png">OR
+					<img src="images/bkash.png">
+					
+					</form>
+				</div>
+		</div>
+			<center><p class="copy">Copyright 2023(jw_developer) . All rights reserved.</p></center>
+		
+		<!-- / container -->
+	</footer>
+	
